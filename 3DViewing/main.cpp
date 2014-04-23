88d0f78e50490941c24d9ad6c0f7ac47f7554e27
Fernando Birra <fernando.birra@gmail.com>
2014-04-23 23:31:03 +0100

    Initial Commit

---
+++ 3DViewing/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  3DViewing
+//
+//  Created by Fernando Birra on 23/04/14.
+//  Copyright (c) 2014 FCT/UNL. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
